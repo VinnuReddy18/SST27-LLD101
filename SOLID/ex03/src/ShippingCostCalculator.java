d558fc16019148e46f7c95437e9d53c83c2b3a7f
VinayReddy18 <142021280+VinnuReddy18@users.noreply.github.com>
2025-08-25 03:43:24 +0530

    ex03- refactored a/c to solid

--- SOLID/ex03/src/ShippingCostCalculator.java
+++ SOLID/ex03/src/ShippingCostCalculator.java
@@ -1,8 +1,16 @@
 public class ShippingCostCalculator {
-    double cost(Shipment s){
-        if ("STANDARD".equals(s.type)) return 50 + 5*s.weightKg;
-        if ("EXPRESS".equals(s.type))  return 80 + 8*s.weightKg;
-        if ("OVERNIGHT".equals(s.type))return 120 + 10*s.weightKg;
-        throw new IllegalArgumentException("Unknown type: " + s.type);
+    double cost(Shipment s) {
+        ShippingStrategy strategy = getStrategy(s.type);
+        return strategy.calculateCost(s.weightKg);
+    }
+
+    private ShippingStrategy getStrategy(String type) {
+        if ("STANDARD".equals(type))
+            return new StandardShipping();
+        if ("EXPRESS".equals(type))
+            return new ExpressShipping();
+        if ("OVERNIGHT".equals(type))
+            return new OvernightShipping();
+        throw new IllegalArgumentException("Unknown type: " + type);
     }
 }
