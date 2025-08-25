5cbc26b6dd4717b71dcf9e4dedbb8c4dd164ce91
VinayReddy18 <142021280+VinnuReddy18@users.noreply.github.com>
2025-08-25 12:35:48 +0530

    ex10- refactored a/c to solid

--- SOLID/ex10/src/Demo10.java
+++ SOLID/ex10/src/Demo10.java
@@ -1,5 +1,11 @@
 public class Demo10 {
     public static void main(String[] args) {
-        new ReportService().generate();
+        Logger consoleLogger = new ConsoleLogger();
+        ReportService service1 = new ReportService(consoleLogger);
+        service1.generate();
+
+        Logger fileLogger = new FileLogger();
+        ReportService service2 = new ReportService(fileLogger);
+        service2.generate();
     }
 }
